64fe4f07abe5f3748b61655ae18e497fcc792df6
AshaOnye <116350271+AshaOnye@users.noreply.github.com>
2024-10-21 22:28:40 +0100

    first change

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view('about');
+}) ->name('about');
+
+Route::get('/gallery', function () {
+    return view('gallery');
+}) ->name('gallery');
+
+Route::get('/contact', function () {
+    return view('contact');
+}) ->name('contact');
+
+
