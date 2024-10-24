db87dc91663d35b6df0556812ab51abbd8461cd8
AshaOnye <116350271+AshaOnye@users.noreply.github.com>
2024-10-24 19:21:23 +0100

    fifth change

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+<title>YLGI | Home</title>
+
+<!-- Fav Icon -->
+<link rel="icon" href="assets/images/favicon-2.png" type="image/x-icon">
+
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400;500;600;700&amp;display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&amp;display=swap" rel="stylesheet">
+
+<!-- Stylesheets -->
+<link href="assets/css/font-awesome-all.css" rel="stylesheet">
+<link href="assets/css/flaticon.css" rel="stylesheet">
+<link href="assets/css/owl.css" rel="stylesheet">
+<link href="assets/css/bootstrap.css" rel="stylesheet">
+<link href="assets/css/jquery.fancybox.min.css" rel="stylesheet">
+<link href="assets/css/animate.css" rel="stylesheet">
+<link href="assets/css/nice-select.css" rel="stylesheet">
+<link href="assets/css/elpath.css" rel="stylesheet">
+<link href="assets/css/color.css" id="jssDefault" rel="stylesheet">
+<link href="assets/css/rtl.css" rel="stylesheet">
+<link href="assets/css/style.css" rel="stylesheet">
+<link href="assets/css/module-css/banner.css" rel="stylesheet">
+<link href="assets/css/module-css/mission.css" rel="stylesheet">
+<link href="assets/css/module-css/about.css" rel="stylesheet">
+<link href="assets/css/module-css/gallery.css" rel="stylesheet">
+<link href="assets/css/module-css/clients.css" rel="stylesheet">
+<link href="assets/css/module-css/conference.css" rel="stylesheet">
+<link href="assets/css/module-css/testimonial.css" rel="stylesheet">
+<link href="assets/css/module-css/cta.css" rel="stylesheet">
+<link href="assets/css/module-css/news.css" rel="stylesheet">
+<link href="assets/css/responsive.css" rel="stylesheet">
+
+</head>
+
+
+<body>
+    <div class="boxed_wrapper ltr">
+        
+
+    </div>
+</body> 
+</html>
