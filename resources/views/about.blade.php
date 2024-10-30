39b12e4fc1579861d1dff4a50b3c6ce98e566958
AshaOnye <116350271+AshaOnye@users.noreply.github.com>
2024-10-30 23:28:46 +0100

    eight

---
+++ resources/views/about.blade.php
@@ -0,0 +1,223 @@
+@extends('layout')
+@section('content')
+<!-- page-title -->
+<section class="page-title">
+            <div class="bg-layer" style="background-image: url(assets/images/background/page-title-4.jpg);"></div>
+            <div class="auto-container">
+                <div class="content-box">
+                    <ul class="bread-crumb clearfix mb_20">
+                        <li><a href="index.html"># Home</a></li>
+                        <li>&nbsp;-&nbsp;</li>
+                        <li>About Us</li>
+                    </ul>
+                    <h1>About Us</h1>
+                </div>
+            </div>
+        </section>
+        <!-- page-title end -->
+
+        <!-- about-section -->
+       <section class="about-section pt_140 pb_140">
+        <div class="pattern-layer">
+            <div class="pattern-1 float-bob-y" style="background-image: url(assets/images/shape/shape-5.png);"></div>
+            <div class="pattern-2" style="background-image: url(assets/images/shape/shape-6.png);"></div>
+        </div>
+        <div class="auto-container">
+            <div class="row clearfix">
+                <div class="col-lg-7 col-md-12 col-sm-12 content-column">
+                    <div class="content-box">
+                        <div class="inner-box mb_40">
+                            <div class="sec-title mb_20">
+                                <span class="sub-title"># Who We Are</span>
+                                <h2>About Us</h2>
+                            </div>
+                            <div class="text-box">
+                                <p>Young Leaders in Governance Initiative (YLGI) is a dynamic non-governmental organization committed to nurturing and empowering the next generation of leaders. Our mission is to foster leadership skills and civic engagement among young people, equipping them with the tools, knowledge, and networks necessary to drive positive change within their communities and beyond.</p>
+                            </div>
+                            <div class="text-box">
+                                <h2>Our Mission</h2>
+                                <p>To develop the next generation of leaders by bringing democratic governance up to date through the alignment of values and principles. Our goal is to establish a platform that will enable future leaders to reach their full potential and make significant contributions to society and governance.</p>
+                            </div>
+                            <div class="text-box">
+                                <h2>Our Vision</h2>
+                                <p>To raise young leaders and influencers for a new generation, capable of transforming governance and fostering sustainable development.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5 col-md-12 col-sm-12 image-column">
+                    <figure class="image-box"><img src="assets/images/resource/about-3.jpg" alt=""></figure>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- about-section end -->
+
+     <!-- mission-section -->
+     <section class="mission-section pt_140 pb_110 centred">
+            <div class="pattern-layer" style="background-image: url(assets/images/shape/shape-11.png);"></div>
+            <div class="auto-container">
+                <div class="title-inner mb_60">
+                    <div class="sec-title centred mb_20">
+                        <span class="sub-title">#Values</span>
+                        <h2>Our Core Values</h2>
+                    </div>
+                    <p>With our passion, knowledge, creative flair and inspiration, <br />we are dedicated in helping.</p>
+                </div>
+                <div class="row clearfix">
+                    <div class="col-lg-4 col-md-6 col-sm-12 mission-block">
+                        <div class="mission-block-one wow fadeInUp animated" data-wow-delay="00ms" data-wow-duration="1500ms">
+                            <div class="inner-box">
+                                <div class="icon-box"><img src="assets/images/icons/icon-3.png" alt=""></div>
+                                <h3><a href="index.html">Service</a></h3>
+                                <p>Service-oriented leadership prioritizes the needs and well-being of others above personal gain. </p>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 mission-block">
+                        <div class="mission-block-one wow fadeInUp animated" data-wow-delay="300ms" data-wow-duration="1500ms">
+                            <div class="inner-box">
+                                <div class="icon-box"><img src="assets/images/icons/icon-3.png" alt=""></div>
+                                <h3><a href="index.html">Integrity</a></h3>
+                                <p>Integrity involves adhering to moral and ethical principles, even when it is challenging. </p>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 mission-block">
+                        <div class="mission-block-one wow fadeInUp animated" data-wow-delay="600ms" data-wow-duration="1500ms">
+                            <div class="inner-box">
+                                <div class="icon-box"><img src="assets/images/icons/icon-3.png" alt=""></div>
+                                <h3><a href="index.html">Resilience</a></h3>
+                                <p>: Resilience is the capacity to recover from setbacks and persist in the face of adversity.</p>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- mission-section end -->
+
+         <!-- event-section -->
+         <section class="event-section pt_140 pb_110">
+            <div class="pattern-layer">
+                <div class="pattern-1" style="background-image: url(assets/images/shape/shape-12.png);"></div>
+                <div class="pattern-2" style="background-image: url(assets/images/shape/shape-13.png);"></div>
+                <div class="pattern-3" style="background-image: url(assets/images/shape/shape-14.png);"></div>
+            </div>
+            <div class="auto-container">
+                <div class="row clearfix">
+                    <div class="col-lg-6 col-md-12 col-sm-12 content-column">
+                        <div class="content-box">
+                            <div class="sec-title mb_20">
+                                <span class="sub-title"># Our Agenda</span>
+                                <h2>Programs and Initiatives</h2>
+                            </div>
+                            <div class="text-box">
+                                <p>With our passion, knowledge, creative flair and inspiration, we are dedicated in helping you to achieve your dream wedding day.</p>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12 col-sm-12 inner-column">
+                        <div class="tabs-box">
+                            <div class="tab-btn-box p_relative">
+                                <ul class="tab-btns tab-buttons clearfix">
+                                    <li class="tab-btn active-btn" data-tab="#tab-1">
+                                        <h3>Workshops</h3>
+                                        <span></span>
+                                    </li>
+                                    <li class="tab-btn" data-tab="#tab-2">
+                                        <h3>Empowerment</h3>
+                                        <span></span>
+                                    </li>
+                                    <li class="tab-btn" data-tab="#tab-3">
+                                        <h3>Projects</h3>
+                                        <span></span>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="tabs-content">
+                                <div class="tab active-tab" id="tab-1">
+                                    <div class="event-content">
+                                        <div class="event-block-one">
+                                            <div class="inner-box">
+                                                <figure class="image-box"><a href="#"><img src="assets/images/resource/event-1.jpg" alt=""></a></figure>
+                                                <div class="inner">
+                                                    <h5><a href="#">Leadership Development Workshops / Conferences</a></h5>
+                                                    <p>Comprehensive workshops focusing on essential leadership skills such as communication, decision-making, and strategic thinking.
+                                                        Guest speakers and mentors from various fields to provide real-world insights and guidance.
+                                                        </p>
+                                                    
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="event-block-one">
+                                            <div class="inner-box">
+                                                <figure class="image-box"><a href="#"><img src="assets/images/resource/event-2.jpg" alt=""></a></figure>
+                                                <div class="inner">
+                                                    <h5><a href="#">Governance Training and Civic Education </a></h5>
+                                                    <p>Programs designed to educate young people about governance structures, civic responsibilities, and public policy.
+                                                        Interactive sessions with policymakers, government officials, and civic leaders.
+                                                       </p>
+                                                    
+                                                </div>
+                                            </div>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                                <div class="tab" id="tab-2">
+                                    <div class="event-content">
+                                        <div class="event-block-one">
+                                            <div class="inner-box">
+                                                <figure class="image-box"><a href="#"><img src="assets/images/resource/event-10.jpg" alt=""></a></figure>
+                                                <div class="inner">
+                                                    <h5><a href="#">Mentorship and Networking Opportunities</a></h5>
+                                                    <p>Connecting young leaders with experienced mentors from diverse sectors.
+                                                        Facilitating networking events and platforms to foster collaboration and peer learning.
+                                                        </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="event-block-one">
+                                            <div class="inner-box">
+                                                <figure class="image-box"><a href="#"><img src="assets/images/resource/event-11.jpg" alt=""></a></figure>
+                                                <div class="inner">
+                                                    <h5><a href="#"> </a>Youth Empowerment and Advocacy</h5>
+                                                    <p>Initiatives aimed at amplifying the voices of young people in policy discussions and decision-making processes.
+                                                        Advocacy campaigns on issues affecting youth, such as education, employment, and social justice.
+                                                        </p>
+                                                    
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="tab" id="tab-3">
+                                    <div class="event-content">
+                                        <div class="event-block-one">
+                                            <div class="inner-box">
+                                                <figure class="image-box"><a href="#"><img src="assets/images/resource/event-12.jpg" alt=""></a></figure>
+                                                <div class="inner">
+                                                    <h5><a href="#">Community Projects and Volunteering</a></h5>
+                                                    <p>Encouraging youth-led community projects that address local needs and promote social responsibility.
+                                                        Volunteer programs that provide hands-on experience in various aspects of governance and community service.
+                                                        </p>
+                                                    
+                                                </div>
+                                            </div>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- event-section end -->
+@endsection
